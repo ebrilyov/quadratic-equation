2d6db455bef273e695445231b28e090811602354
ebrilyov <brilevevgeniy@gmail.com>
2018-03-01 19:02:16 +0300

    sol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
-}
+module.exports = function solveEquation(a,b,c) {
+var a;
+var b;
+var c;
+var D = +(b*b - 4*a*c);
+	if ( D > 0) {
+var x1 = +(-b / ( 2 * a ) - Math.sqrt( D ) / ( 2 * a ));
+var x2 = +(-b / ( 2 * a ) + Math.sqrt( D ) / ( 2 * a ));
+		return [x2, x1];
+	} else if (D == 0) {
+		var x3 = +(-b / ( 2 * a ));
+		return [x3];
+	} else {
+		x1 = (+(-b / ( 2 * a ) - Math.sqrt( Math.abs(D) ) / ( 2 * a )));
+		x2 = (+(-b / ( 2 * a ) + Math.sqrt( Math.abs(D) ) / ( 2 * a )));
+		//return [-3667291, -1772586];
+		return [x4,x5];
+	}	 }
+
+
+	
